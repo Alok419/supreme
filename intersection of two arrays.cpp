574ce95a085e4a8e75b81fb59c300e3acb11c0fc
Alok419 <109036005+Alok419@users.noreply.github.com>
2023-02-15 09:56:56 +0530

    Add files via upload

---
+++ intersection of two arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+#include<limits.h>
+using namespace std;
+
+int main() {
+  vector<int> arr{1,2,3,4,5, 6,7};
+  vector<int> brr{3,4,10};
+  vector<int> ans;
+  for(int i = 0;i<arr.size();i++){
+    int element = arr[i];
+    for(int j =0; j<brr.size();j++){
+      if(element == brr[j]){
+        brr[j] = INT_MIN;
+      ans.push_back(element);
+      }
+    }
+  }
+
+for(auto val : ans){
+    cout << val << " ";
+}
+  return 0;
+}
